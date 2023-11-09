648f09b8641938e8e51ae0a377633d0e5c6f21a9
gymbundye <84460968+gymbundye@users.noreply.github.com>
2023-11-09 12:11:35 -0500

    Add files via upload

---
+++ Monster/Monster/Data/Monster.cs
@@ -0,0 +1,12 @@
+﻿namespace Monster.Data
+{
+    public class Monster
+    {
+        public int Id { get; set; } 
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;}
+        public string? Alias { get; set; }
+        public string? Location { get; set; }
+        public string? Avatar { get; set; }
+    }
+}
