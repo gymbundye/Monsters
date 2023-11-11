3d11b77ba8f5331dc24005bcd12faa26ef49597e
gymbundye <84460968+gymbundye@users.noreply.github.com>
2023-11-10 21:39:32 -0500

    worked on datacontext

---
+++ Monster/Monster/Data/MonsterContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Monster.Data
+{
+    public class MonsterDataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public MonsterDataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Configuration.GetConnectionString("MonsterDB"));
+        }
+
+        public DbSet<Monster> Employees { get; set; }
+    }
+}
--- Monster/Monster/Program.cs
+++ Monster/Monster/Program.cs
@@ -1,12 +1,19 @@
+using Monster.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Monster.Data;
+
+
+
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("MonsterDB");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContextFactory<MonsterDataContext>(options => options.UseSqlite(connectionString));
 builder.Services.AddSingleton<WeatherForecastService>();
 
 var app = builder.Build();
